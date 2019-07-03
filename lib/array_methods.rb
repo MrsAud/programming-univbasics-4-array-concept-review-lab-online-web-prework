6b099d07b58b489d9f21f9344acc4f97c19c43d4
Audrea Cook <audreacwork@gmail.com>
2019-07-03 20:17:33 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,7 @@
 def find_element_index(array, value_to_find)
   i = 0
   if array.include? (value_to_find)
-    while value_to_find >= array[i] do
+    while value_to_find > array[i] do
     i += 1
     end
     i
