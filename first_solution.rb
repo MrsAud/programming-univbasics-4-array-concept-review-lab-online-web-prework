79fa8d63bfbbd5eb0a2f08fdd80b68e14fcbe639
Audrea Cook <audreacwork@gmail.com>
2019-07-03 19:59:34 +0000

    Automatically backed up by Learn

---
+++ first_solution.rb
@@ -0,0 +1,18 @@
+def find_element_index(array, value_to_find)
+  array.length.times do |count|
+    if array[count] == value_to_find
+      return count
+    end
+  end
+nil
+end
+
+def find_max_value(array)
+  array.sort!
+  p array[-1]
+end
+
+def find_min_value(array)
+  array.sort!
+  p array[0]
+end
